2aa45544883a2c41bf4a03e671bdb700e12729b9
naotakak <naotakakinoshita@gmail.com>
2017-12-14 18:05:05 -0500

    done

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,25 @@
 #include "pipe_networking.h"
-
+#include <ctype.h>
 
 int main() {
 
   int to_client;
   int from_client;
+  //int i;
+  char text[BUFFER_SIZE];
+
+  printf("Server started, waiting for handshake...\n");
 
   from_client = server_handshake( &to_client );
+  while (1) {
+    read(from_client, text, sizeof(text));
+    printf("TEXT READ: %s\n", text);
+    char *s = text;
+    while(*s) {
+      *s = toupper(*s);
+      s ++;
+    }
+    printf("TEXT TO SEND: %s\n", text);
+    write(to_client, text, sizeof(text));
+  }
 }
--- client.c
+++ client.c
@@ -5,6 +5,18 @@ int main() {
 
   int to_server;
   int from_server;
+  char text[BUFFER_SIZE];
 
   from_server = client_handshake( &to_server );
+
+  while (1) {
+    printf("Enter text: ");
+    fgets(text, sizeof(text), stdin);
+    text[strlen(text) - 1] = '\0';
+
+    write(to_server, text, sizeof(text));
+    read(from_server, text, sizeof(text));
+    printf("RESPONSE: %s\n", text);
+  }
+  return 0;
 }
